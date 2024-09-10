69d21f681be1579beb86ad534e29a1af71a80552
T.Aoyama <aoym@issp.u-tokyo.ac.jp>
2024-09-10 12:36:35 +0900

    update version information

--- src/getcif/__init__.py
+++ src/getcif/__init__.py
@@ -4,7 +4,7 @@ from importlib.metadata import PackageNotFoundError, version
 from .main import QueryMaterialsProject
 
 try:
-    __version__ = version("htp-tools-getcif")
+    __version__ = version("HTP-tools-cif2x")
 except PackageNotFoundError:
     # package not installed
     pass
